9cfc8157fb280992cb8aaa78d5eb9918fa32cdb6
behrooz bozorg chami <behroozbc@outlook.com>
2025-12-01 17:26:34 +0100

    vector search

--- main.py
+++ main.py
@@ -1,68 +1,55 @@
+import time
 from dotenv import load_dotenv
 import os
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_postgres import PGVector
 from langchain_core.documents import Document
 from langchain.agents.middleware import dynamic_prompt, ModelRequest
 from langchain.agents import create_agent
-
+from langchain_ollama import OllamaEmbeddings,ChatOllama
+from data import COURSE_URI, extract_html_titles, fetch_toc
 # Load environment variables from .env file
 load_dotenv()
-
+GETDATA=False
 api_key=os.getenv("API_KEY")
 connection=os.getenv("ConnectionString")
 collection_name = os.getenv("collection_name")
-embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001",google_api_key=api_key)
+# embeddings = RateLimitedEmbeddings(model="models/gemini-embedding-001",google_api_key=api_key)
+embeddings = OllamaEmbeddings(
+    model="mxbai-embed-large:latest",  # Replace with your pulled model
+    base_url="http://localhost:11434",  # Default Ollama URL
+    # Optional: Advanced options
+    # show_alternate_urls: False,
+    # threads: 4,  # Number of threads for computation
+)
+model = ChatOllama(
+    model="llama3.1:8b",
+    temperature=0,
+    # other params...
+)
+print(connection)
 vector_store = PGVector(
     embeddings=embeddings,
     collection_name=collection_name,
     connection=connection,
     use_jsonb=True,
 )
-docs = [
-    Document(
-        page_content="there are cats in the pond",
-        metadata={"id": 1, "uri": "bing.com", "topic": "animals"},
-    ),
-    Document(
-        page_content="ducks are also found in the pond",
-        metadata={"id": 2, "location": "pond", "topic": "animals"},
-    ),
-    Document(
-        page_content="fresh apples are available at the market",
-        metadata={"id": 3, "location": "market", "topic": "food"},
-    ),
-    Document(
-        page_content="the market also sells fresh oranges",
-        metadata={"id": 4, "location": "market", "topic": "food"},
-    ),
-    Document(
-        page_content="the new art exhibit is fascinating",
-        metadata={"id": 5, "location": "museum", "topic": "art"},
-    ),
-    Document(
-        page_content="a sculpture exhibit is also at the museum",
-        metadata={"id": 6, "location": "museum", "topic": "art"},
-    ),
-    Document(
-        page_content="a new coffee shop opened on Main Street",
-        metadata={"id": 7, "location": "Main Street", "topic": "food"},
-    ),
-    Document(
-        page_content="the book club meets at the library",
-        metadata={"id": 8, "location": "library", "topic": "reading"},
-    ),
-    Document(
-        page_content="the library hosts a weekly story time for kids",
-        metadata={"id": 9, "location": "library", "topic": "reading"},
-    ),
-    Document(
-        page_content="a cooking class for beginners is offered at the community center",
-        metadata={"id": 10, "location": "community center", "topic": "classes"},
-    ),
-]
 
-vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
+if GETDATA:
+ files = set()
+ toc_html = fetch_toc(COURSE_URI)
+ titles_with_html = []
+ uri_content_list = []
+ extract_html_titles(toc_html, titles_with_html, files, uri_content_list, COURSE_URI)
+ docs = [
+     Document(page_content=item["content"], metadata={"id": idx,"uri":item["uri"]})
+     for idx, item in enumerate(uri_content_list)
+ ]
+ print(len(docs))
+ vector_store.add_documents(docs, ids=[doc.metadata["id"] for doc in docs])
+ print("finished")
+
+
 @dynamic_prompt
 def prompt_with_context(request: ModelRequest) -> str:
     """Inject context into state messages."""
@@ -70,7 +57,7 @@ def prompt_with_context(request: ModelRequest) -> str:
     retrieved_docs = vector_store.similarity_search(last_query)
 
     docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
-
+    print(docs_content)
     system_message = (
         "You are a helpful assistant. Use the following context in your response:"
         f"\n\n{docs_content}"
@@ -79,7 +66,7 @@ def prompt_with_context(request: ModelRequest) -> str:
     return system_message
 
 agent = create_agent(model, tools=[], middleware=[prompt_with_context])
-query = "What is task decomposition?"
+query = "What is ai agents?"
 for step in agent.stream(
     {"messages": [{"role": "user", "content": query}]},
     stream_mode="values",
