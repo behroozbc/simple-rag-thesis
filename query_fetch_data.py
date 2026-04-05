986454532c656a7026ab80d668626b7bc877aa52
priyankatejaravi <priyankatejaravi@gmail.com>
2026-04-05 10:25:19 +0000

    remove if not

--- query_fetch_data.py
+++ query_fetch_data.py
@@ -289,7 +289,7 @@ def fetch_all_documents(nodes, max_workers=32, per_uri_workers=32):
     with ThreadPoolExecutor(max_workers=max_workers) as ex:
         futures = [ex.submit(work, uri) for uri in uris]
         for i, fut in enumerate(as_completed(futures), 1):
-            if not uri:
+            if uri:
                 uri, content, symbols, prereqs = fut.result()
                 nodes[uri]["content"] = content
                 nodes[uri]["symbols"] = symbols if symbols is not None else []
